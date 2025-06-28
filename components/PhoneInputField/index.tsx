02a94280632d05f9f7fcf1cbf98835e07139c85c
Michael Dawson <m.dawson@y7mail.com>
2025-06-28 11:53:03 -0500

    fixup! Update to supported / forked package for phone input

--- components/PhoneInputField/index.tsx
+++ components/PhoneInputField/index.tsx
@@ -1,6 +1,9 @@
 import { COLORS } from "@/styles/colors";
+import PhoneInput, {
+  PhoneInputProps,
+} from "@linhnguyen96114/react-native-phone-input";
 import React, { forwardRef, useImperativeHandle } from "react";
-import PhoneInput, { PhoneInputProps } from "react-native-phone-number-input";
+
 import { View } from "tamagui";
 import { FormControl } from "../FormControl";
 
@@ -16,75 +19,79 @@ interface Props {
   required?: boolean;
 }
 
-export const PhoneInputField = forwardRef<PhoneInput, Props>((props, ref) => {
-  const phoneInputRef = React.useRef<PhoneInput>(null);
+export const PhoneInputField = forwardRef<typeof PhoneInput, Props>(
+  (props, ref) => {
+    const phoneInputRef = React.useRef(null);
 
-  // Forward the ref to the PhoneInput component
-  useImperativeHandle(ref, () => phoneInputRef.current!);
+    // Forward the ref to the PhoneInput component
+    useImperativeHandle(ref, () => phoneInputRef.current!);
 
-  const {
-    defaultValue,
-    defaultCode = "US",
-    layout = "first",
-    onChangeText,
-    onChangeFormattedText,
-    error,
-    touched,
-    label,
-    required,
-  } = props;
+    const {
+      defaultValue,
+      defaultCode = "US",
+      layout = "first",
+      onChangeText,
+      onChangeFormattedText,
+      error,
+      touched,
+      label,
+      required,
+    } = props;
 
-  return (
-    <FormControl
-      error={error}
-      touched={touched}
-      label={label}
-      required={required}
-    >
-      <View
-        borderWidth={1}
-        borderColor={touched && error ? COLORS.error : COLORS.primary}
-        borderRadius={10}
-        overflow="hidden"
+    return (
+      <FormControl
+        error={error}
+        touched={touched}
+        label={label}
+        required={required}
       >
-        {/* @ts-ignore-line - bug in the library */}
-        <PhoneInput
-          ref={phoneInputRef}
-          defaultValue={defaultValue}
-          defaultCode={defaultCode}
-          layout={layout}
-          onChangeText={onChangeText}
-          onChangeFormattedText={onChangeFormattedText}
-          containerStyle={{
-            width: "100%",
-            paddingLeft: 10,
-            height: 48,
-            borderWidth: 0, // Remove the internal borders since we have one on the container
-            backgroundColor: COLORS.background,
-          }}
-          flagButtonStyle={{
-            padding: 0,
-            width: "auto",
-          }}
-          textContainerStyle={{
-            paddingRight: 0,
-            paddingVertical: 0,
-            backgroundColor: COLORS.background,
-          }}
-          textInputStyle={{
-            height: 50,
-            marginLeft: 10,
-            borderBottomWidth: 0,
-            paddingHorizontal: 16,
-            paddingVertical: 0,
-            fontSize: 16,
-            color: COLORS.text,
-          }}
-          codeTextStyle={{
-            color: COLORS.text,
-          }}
-        />
-      </View>
-    </FormControl>
-  );
-});
+        <View
+          borderWidth={1}
+          borderColor={touched && error ? COLORS.error : COLORS.primary}
+          borderRadius={10}
+          overflow="hidden"
+        >
+          <PhoneInput
+            ref={phoneInputRef}
+            defaultValue={defaultValue}
+            defaultCode={defaultCode}
+            layout={layout}
+            onChangeText={onChangeText}
+            onChangeFormattedText={onChangeFormattedText}
+            containerStyle={{
+              width: "100%",
+              paddingLeft: 10,
+              height: 48,
+              borderWidth: 0, // Remove the internal borders since we have one on the container
+              backgroundColor: COLORS.background,
+            }}
+            placeholder="Enter your phone number"
+            flagButtonStyle={{
+              padding: 0,
+              width: "auto",
+            }}
+            textContainerStyle={{
+              paddingRight: 0,
+              paddingVertical: 0,
+              backgroundColor: COLORS.background,
+            }}
+            textInputStyle={{
+              height: 50,
+              marginLeft: 5,
+              borderBottomWidth: 0,
+              paddingVertical: 0,
+              fontSize: 16,
+              color: COLORS.text,
+            }}
+            textInputProps={{
+              placeholderTextColor: COLORS.textMuted,
+            }}
+            codeTextStyle={{
+              color: COLORS.text,
+            }}
+          />
+        </View>
+      </FormControl>
+    );
+  }
+);
